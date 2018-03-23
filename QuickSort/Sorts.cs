73cb58ddadc16be2005b1942c535f8cbfd74eca7
lexsirius <abry2@mail.ru>
2018-03-23 19:59:41 +0500

    Добавьте файлы проекта.

---
+++ QuickSort/Sorts.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorts
+{
+    public class Sort
+    {
+        public static void QuickSort(int[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                var p = Partition(array, left, right);
+                QuickSort(array, left, p - 1);
+                QuickSort(array, p, right);
+            }
+        }
+
+        private static int Partition(int[] array, int left, int right)
+        {
+            var pivot = array[left];
+            var i = left;
+            var j = right;
+            var len = array.Length;
+            while (i <= j)
+            {
+                while (array[i] < pivot && i < len - 1)
+                    i++;
+                while (array[j] > pivot && j > 0)
+                    j--;
+                if (i <= j)
+                {
+                    var container = array[i];
+                    array[i] = array[j];
+                    array[j] = container;
+                    i++;
+                    j--;
+                }
+            }
+            return i;
+        }
+    }
+}
---
+++ QuickSortTests/UnitTestSort.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sorts;
+
+namespace QuickSortTests
+{
+    [TestClass]
+    public class UnitTestSort
+    {
+        [TestMethod]
+        public void ThreeElements()
+        {
+            var array = new[] { 5, 1, 3 };
+            Sort.QuickSort(array, 0, array.Length - 1);
+            Assert.IsTrue(IsSorted(array));
+        }
+
+        [TestMethod]
+        public void HundredIndentialElements()
+        {
+            var array = new int[100];
+            var len = array.Length;
+            for (int i = 0; i < len; i++)
+                array[i] = 0;
+            Sort.QuickSort(array, 0, len - 1);
+            Assert.IsTrue(IsSorted(array));
+        }
+
+        [TestMethod]
+        public void ThousandRandomElements()
+        {
+            var rnd = new Random();
+            var array = new int[1000];
+            var len = array.Length;
+            for (int i = 0; i < len; i++)
+                array[i] = rnd.Next(0, 1000);
+            Sort.QuickSort(array, 0, len - 1);
+            Assert.IsTrue(IsSortedRndCheck(array));
+        }
+
+        [TestMethod]
+        public void IsEmpty()
+        {
+            var array = new int[0];
+            Sort.QuickSort(array, 0, array.Length - 1);
+            Assert.IsTrue(IsSorted(array));
+        }
+
+        [TestMethod]
+        public void BigArray()
+        {
+            var rnd = new Random();
+            var array = new int[100000000];
+            var len = array.Length;
+            for (int i = 0; i < len; i++)
+                array[i] = rnd.Next(0, 100000);
+            Sort.QuickSort(array, 0, len - 1);
+            Assert.IsTrue(IsSorted(array));
+        }
+
+        private static bool IsSorted(int[] array)
+        {
+            var len = array.Length;
+            for (int i = 1; i < len; i++)
+            {
+                if (array[i - 1] > array[i])
+                    return false;
+            }
+            return true;
+        }
+
+        private static bool IsSortedRndCheck(int[] array)
+        {
+            var rnd = new Random();
+            var len = array.Length;
+            for (int i = 0; i < 10; i++)
+            {
+                var jLeft = rnd.Next(0, len / 2);
+                var jRight = rnd.Next(len / 2 + 1, len - 1);
+                if (array[jLeft] > array[jRight])
+                    return false;
+            }
+            return true;
+        }
+
+    }
+}
